063eb341ce480e22f7356a27a6e513791467ff3d
FémLol Stúdió <femlol1femlol@gmail.com>
2023-12-04 14:36:22 +0100

    fixing windows problem

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,52 @@
-use inline_colorization::*;
+use colored::*;
 use std::time::Duration;
-
 use tokio::time::sleep;
 
 #[tokio::main]
 async fn main() {
-    println!("{color_green}Meow");
-    sleep(Duration::from_millis(800)).await;
-    println!("{color_blue}Meow");
-    sleep(Duration::from_millis(800)).await;
-    println!("{color_magenta}I'm a cow");
-    sleep(Duration::from_millis(800)).await;
-    println!("{color_green}Meow");
-    sleep(Duration::from_millis(300)).await;
-    println!("{color_blue}Meow");
-    sleep(Duration::from_millis(300)).await;
-    println!("{color_magenta}I'm a cow");
-    sleep(Duration::from_millis(600)).await;
-    if let Err(error) = open::that("https://www.youtube.com/watch?v=CZlfbep2LdU") {
-        println!("{color_red}Can't open URL: {}", error.to_string());
+    match enable_ansi_support::enable_ansi_support() {
+        Ok(()) => {
+            // ANSI escape codes were successfully enabled, or this is a non-Windows platform.
+            // Use your terminal color library of choice here.
+            println!("{}", "Meow".green());
+            sleep(Duration::from_millis(800)).await;
+            println!("{}", "Meow".blue());
+            sleep(Duration::from_millis(800)).await;
+            println!("{}", "I'm a cow".magenta());
+            sleep(Duration::from_millis(800)).await;
+            println!("{}", "Meow".green());
+            sleep(Duration::from_millis(300)).await;
+            println!("{}", "Meow".blue());
+            sleep(Duration::from_millis(300)).await;
+            println!("{}", "I'm a cow".magenta());
+            sleep(Duration::from_millis(600)).await;
+            if let Err(error) = open::that("https://www.youtube.com/watch?v=CZlfbep2LdU") {
+                println!(
+                    "{}: {}",
+                    "Can't open URL".red(),
+                    error.to_string().bright_red()
+                );
+            }
+        }
+        Err(_) => {
+            // The operation was unsuccessful, typically because it's running on an older
+            // version of Windows. The program may choose to disable ANSI color code output in
+            // this case.
+            println!("Meow");
+            sleep(Duration::from_millis(800)).await;
+            println!("Meow");
+            sleep(Duration::from_millis(800)).await;
+            println!("I'm a cow");
+            sleep(Duration::from_millis(800)).await;
+            println!("Meow");
+            sleep(Duration::from_millis(300)).await;
+            println!("Meow");
+            sleep(Duration::from_millis(300)).await;
+            println!("I'm a cow");
+            sleep(Duration::from_millis(600)).await;
+            if let Err(error) = open::that("https://www.youtube.com/watch?v=CZlfbep2LdU") {
+                println!("Can't open URL: {}", error.to_string());
+            }
+        }
     }
-    println!("{color_white}");
 }
